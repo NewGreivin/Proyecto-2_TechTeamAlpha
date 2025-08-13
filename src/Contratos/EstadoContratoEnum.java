610535be016fecfc7c486f9ba78fac1a9f3c99d9
marisolalfaro <marialfarolo@est.unt.ac.cr>
2025-08-12 20:43:55 -0600

    Se crea el EstadoContratoEnum
    posee sus atributos y metodos

---
+++ src/Contratos/EstadoContratoEnum.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package Contratos;
+
+/**
+ *
+ * @author Marisol
+ */
+public enum EstadoContratoEnum {
+    ACTIVO("Activo"),
+    FINALIZADO("Finalizado"),
+    CANCELADO("Cancelado");
+    
+    private final String descripcion;
+
+    private EstadoContratoEnum(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "EstadoContratoEnum{" + "ordinal=" + ordinal() + ", name=" + name() + ", descripcion=" + descripcion + '}';
+    }
+    
+}

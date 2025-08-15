760e185b8f1baa5a40a3dfeb1e7c144280e8d3ca
Ricardo <richavesca@est.utn.ac.cr>
2025-08-14 21:05:59 -0600

    Se creo el paquete Empleados y la clase Empleado y la clase GestionEmpleado
    Se creo la clase Empleado con tus metodos, constructor y toString, se creo la clase Gestion con sus metodos, agregar, borrar, actualizar y buscar

---
+++ src/Empleados/Empleado.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Empleados;
+
+import Personas.Persona;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Ricardo Chaves
+ */
+public class Empleado extends Persona  {
+    private String puesto;
+    private double salario;
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public Empleado(String cedula, String nombre, LocalDate fechaNacimiento, String telefono, String correo, String puesto, double salario) {
+        super(cedula, nombre, fechaNacimiento, telefono, correo);
+        this.puesto = puesto;
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" + super.toString() + "puesto=" + puesto + ", salario=" + salario + '}';
+    }
+    
+}
---
+++ src/Empleados/GestionEmpleado.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Empleados;
+
+import Interfaces.IGestionDatos;
+import Utilidades.UtilValidaciones;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Ricardo Chaves
+ */
+public class GestionEmpleado implements IGestionDatos <Empleado>{
+    private ArrayList<Empleado> Empleado = new ArrayList<>();
+
+    @Override
+    public boolean agregar(Empleado t) {
+        if(buscar(t.getCedula())!=null){
+            return false;
+        }
+        
+        if(UtilValidaciones.calcularEdad(t.getFechaNacimiento())<18){
+            return false;
+        }
+        
+        if(UtilValidaciones.validarTelefono(t.getTelefono())){
+            return false;
+        }
+        
+        if(UtilValidaciones.validarCorreo(t.getCorreo())){
+            return false;
+        }
+        
+        if(Empleado.add(t));
+        return true;
+    }
+
+    @Override
+    public Empleado buscar(Object t) {
+        for(Empleado e:Empleado){
+            if(e.getCedula().equals(t)){
+                return e;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean actualizar(Empleado t) {
+        Empleado existe = buscar(t.getCedula());
+            if(existe == null){
+                return false;
+            }
+        
+        existe.setPuesto(t.getPuesto());
+        existe.setCorreo(t.getCorreo());
+        existe.setTelefono(t.getTelefono());
+        return true;
+        
+    }
+
+    @Override
+    public boolean eliminar(Empleado t) {
+        return Empleado.remove(t);
+    }
+    
+    
+}

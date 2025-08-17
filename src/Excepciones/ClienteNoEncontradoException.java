450546cbb932d100680b37adf161270f541acf45
GenesisJimenezZZ <gejimenezfl@est.utn.ac.cr>
2025-08-17 12:52:00 -0600

    Se crea el paquete excepciones
    Se crea la clase clientes Excepcion

--- src/Excepciones/ClienteNoEncontradoException.java
+++ src/Excepciones/ClienteNoEncontradoException.java
@@ -2,14 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Excepciones.vehiculos;
+package Excepciones;
 
 /**
  *
- * @author gene
+ * @author Usuario
  */
-public class VehiculoEnAlquilerException extends Exception {
-    public VehiculoEnAlquilerException(String mensaje) {
+public class ClienteNoEncontradoException extends Exception {
+    public ClienteNoEncontradoException(String mensaje) {
         super(mensaje);
     }
 }

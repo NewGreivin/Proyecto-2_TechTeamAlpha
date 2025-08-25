44497cdfcc13e6d40e3f5200333a857d7fffaabe
marisolalfaro <marialfarolo@est.unt.ac.cr>
2025-08-24 18:36:57 -0600

    Se actualiza la clase GestionContrato
    Se agrega metodo en agregar

--- src/Contratos/GestionContrato.java
+++ src/Contratos/GestionContrato.java
@@ -5,6 +5,7 @@
 package Contratos;
 
 import Interfaces.IGestionDatos;
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 
@@ -15,7 +16,7 @@ import java.util.HashMap;
 public class GestionContrato implements IGestionDatos<Contrato> {
     private HashMap<String, Contrato> contratos;
 
-    public GestionContrato(HashMap<String, Contrato> contratos) {
+    public GestionContrato() {
         this.contratos = new HashMap<>();
     }
 
@@ -24,6 +25,33 @@ public class GestionContrato implements IGestionDatos<Contrato> {
         if(contratos.containsKey(contrato.getNumContrato())){
             return false;
         }
+        
+        if(contrato.getCliente() == null){
+            return false;
+        }
+        
+        if(contrato.getVehiculo() == null){
+            return false;
+        }
+        
+        if(contrato.getFechaInicio().isBefore(LocalDate.now())){
+            return false;
+        }
+        
+        if(contrato.getFechaFin().isBefore(contrato.getFechaInicio())){
+            return false;
+        }
+        
+        for(Contrato c : contratos.values()){
+            if(c.getVehiculo().equals(contrato.getVehiculo())){
+                boolean solapado = !(contrato.getFechaFin().isBefore(c.getFechaInicio())
+                        || contrato.getFechaInicio().isAfter(c.getFechaFin()));
+                if (solapado){
+                    return false;
+                }
+            }
+        }
+        
         contratos.put(contrato.getNumContrato(), contrato);
         return true;
     }

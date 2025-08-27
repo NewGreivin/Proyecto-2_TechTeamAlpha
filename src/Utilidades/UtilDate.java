a4ea5e19486660233bab1a64989246f6bf21c7fd
Ricardo <richavesca@est.utn.ac.cr>
2025-08-26 21:36:21 -0600

    Agrego los UtilDate
    Carga de metodos de validaciones de fechas que no se cargaron en el commit anterior

--- src/Utilidades/UtilDate.java
+++ src/Utilidades/UtilDate.java
@@ -4,10 +4,19 @@
  */
 package Utilidades;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  *
  * @author Ricardo Chaves
  */
 public class UtilDate {
+    public static LocalDate toLocalDate(String date){
+        return LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    }
     
+    public static String toString(LocalDate date){
+        return date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    }
 }
